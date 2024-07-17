9fa8be00b0a87647728e38814af835966cc3589b
MarosVi <116791998+MarosVi@users.noreply.github.com>
2024-07-17 20:03:06 +0200

    Id 92 (#15)
    * ID-92 ID-SK Dropdown
    
    * ID-SK Header fix
    
    * ID-SK Header fix
    
    * Trying to fix styles
    
    * Fix issue with styles for dropdown component
    
    ---------
    
    Co-authored-by: Sobanim <sobolev.dmirtiy@gmail.com>

--- CHANGELOG-IDSK.md
+++ CHANGELOG-IDSK.md
@@ -11,6 +11,7 @@
 - _[ID-100]_: ID-SK Card
 - _[ID-85]_: ID-SK Header
 - _[ID-93]_: ID-SK File upload
+- _[ID-92]_: ID-SK Dropdown
 - _[ID-87]_: ID-SK Feedback bar
 - _[ID-94]_: ID-SK Error summary
 - _[ID-84]_: ID-SK Input
--- packages/idsk-frontend/src/govuk/all.jsdom.test.mjs
+++ packages/idsk-frontend/src/govuk/all.jsdom.test.mjs
@@ -10,6 +10,7 @@ jest.mock(`./components/accordion/accordion.mjs`)
 jest.mock(`./components/button/button.mjs`)
 jest.mock(`./components/character-count/character-count.mjs`)
 jest.mock(`./components/checkboxes/checkboxes.mjs`)
+jest.mock(`./components/dropdown/dropdown.mjs`)
 jest.mock(`./components/error-summary/error-summary.mjs`)
 jest.mock(`./components/exit-this-page/exit-this-page.mjs`)
 jest.mock(`./components/header/header.mjs`)
@@ -23,6 +24,7 @@ jest.mock(`./components/textarea/textarea.mjs`)
 describe('initAll', () => {
   const components = [
     'checkboxes',
+    'dropdown',
     'header',
     'radios',
     'skip-link',
